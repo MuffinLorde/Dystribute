7c2fed4b2ee5638990f412bbd573873a75bd2fb0
SoapyTheToast <scromskrom@gmail.com>
2024-04-14 18:20:28 +0200

    made a multitude of changes

--- src/import_file.py
+++ src/import_file.py
@@ -3,3 +3,14 @@ import os
 
 def import_package(package_link):
     os.system(f'git clone {package_link}')
+
+    with open('dyst.config', 'a') as main_file:
+        count = 0
+
+        lines = main_file.readlines()
+
+        for line in lines:
+            count += 1
+            line_strip = line.strip()
+
+            print(line_strip)
